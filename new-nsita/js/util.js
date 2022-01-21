8198248c4091cda727521dc9190b61cf735d78f3
wanqqq29 <wanqqq29@gmail.com>
2022-01-21 16:21:08 +0800

    简介上传

--- new-nsita/js/util.js
+++ new-nsita/js/util.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wanqqq29
  * @Date: 2022-01-21 11:23:37
- * @LastEditTime: 2022-01-21 15:37:33
+ * @LastEditTime: 2022-01-21 16:14:26
  * @LastEditors: wanqqq29
  * @Description: blog.wanqqq29.cn
  * @FilePath: \1x3Daily-HTML\new-nsita\js\util.js
@@ -13,7 +13,7 @@ function change(i) {
     $(".origin").children().eq(i).css("display", "none");
     $(".origin").children().eq(i + 1).css({
         "display": "block",
-        "animation": "star 1.1s cubic-bezier(0.4, 0, 1, 1) "
+        "animation": "star 0.4s cubic-bezier(0.4, 0, 1, 1) "
     });
 }
 
@@ -21,7 +21,7 @@ function change_title() {
     var i = -1;
     $(".origin").children().eq(0).css({
         "display": "block",
-        "animation": "star 1.1s cubic-bezier(0.4, 0, 1, 1) "
+        "animation": "star 0.4s cubic-bezier(0.4, 0, 1, 1) "
     });
     setInterval(() => {
         change(i);
@@ -29,5 +29,5 @@ function change_title() {
         if (i == 4) { //判断当i==4（一次循环完毕后从-1开始继续循环）
             i = -1
         }
-    }, 3000);
+    }, 2000);
 }
