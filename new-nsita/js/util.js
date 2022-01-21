388c4396cd6e51120514a12e03679e2a5e9d6571
wanqqq29 <wanqqq29@gmail.com>
2022-01-21 15:39:03 +0800

    标题轮换添加动画

--- new-nsita/js/util.js
+++ new-nsita/js/util.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wanqqq29
  * @Date: 2022-01-21 11:23:37
- * @LastEditTime: 2022-01-21 15:11:30
+ * @LastEditTime: 2022-01-21 15:37:33
  * @LastEditors: wanqqq29
  * @Description: blog.wanqqq29.cn
  * @FilePath: \1x3Daily-HTML\new-nsita\js\util.js
@@ -11,18 +11,23 @@ change_title()
 // 显示element i传值 把当前隐藏并显示下一个
 function change(i) {
     $(".origin").children().eq(i).css("display", "none");
-    $(".origin").children().eq(i + 1).css("display", "block");
+    $(".origin").children().eq(i + 1).css({
+        "display": "block",
+        "animation": "star 1.1s cubic-bezier(0.4, 0, 1, 1) "
+    });
 }
 
 function change_title() {
     var i = -1;
-    $(".origin").children().eq(0).css("display", "block");
+    $(".origin").children().eq(0).css({
+        "display": "block",
+        "animation": "star 1.1s cubic-bezier(0.4, 0, 1, 1) "
+    });
     setInterval(() => {
         change(i);
         i++;
         if (i == 4) { //判断当i==4（一次循环完毕后从-1开始继续循环）
             i = -1
         }
-    }, 1500);
-
+    }, 3000);
 }
