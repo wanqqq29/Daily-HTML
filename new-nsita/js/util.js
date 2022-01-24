f1b211ac8dbd2b2f07bdb79ca928f6035a2a8879
wanqqq29 <wanqqq29@gmail.com>
2022-01-24 16:55:25 +0800

    添加section3导航 实现hover切换 todo:内容块

--- new-nsita/js/util.js
+++ new-nsita/js/util.js
@@ -1,13 +1,13 @@
 /*
  * @Author: wanqqq29
  * @Date: 2022-01-21 11:23:37
- * @LastEditTime: 2022-01-21 16:14:26
+ * @LastEditTime: 2022-01-24 15:31:39
  * @LastEditors: wanqqq29
  * @Description: blog.wanqqq29.cn
  * @FilePath: \1x3Daily-HTML\new-nsita\js\util.js
  */
 change_title()
-
+tab_change()
 // 显示element i传值 把当前隐藏并显示下一个
 function change(i) {
     $(".origin").children().eq(i).css("display", "none");
@@ -30,4 +30,13 @@ function change_title() {
             i = -1
         }
     }, 2000);
+}
+
+//跟随鼠标切换tab
+// jquery事件 mouseenter(鼠标移入事件)
+// 鼠标移入触发.tab('show')
+function tab_change(){
+    $('.nav_btn').mouseenter(function(event){
+        $(this).tab('show')
+    })
 }
